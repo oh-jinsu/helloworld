f8487671a7097293590b00212c5ad0789c5ed8c3
오진수 <ohjinsu@codersproduct.com>
2022-03-25 22:30:44 +0900

    Add test cases

--- test/sign_in_test.go
+++ test/sign_in_test.go
@@ -74,3 +74,75 @@ func TestSignIn(t *testing.T) {
 
 	assert.NotEmpty(t, resBody.RefreshToken)
 }
+
+func TestSignInWithWrongUsername(t *testing.T) {
+	db := NewTestDB(&models.User{})
+
+	defer db.Close()
+
+	client := NewSignInTestClient(db)
+
+	defer client.Close()
+
+	reqBody1 := gin.H{
+		"username": "username",
+		"password": "password",
+	}
+
+	res1, err := client.Request("auth", http.MethodPost, reqBody1)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	assert.Equal(t, http.StatusCreated, res1.StatusCode)
+
+	reqBody2 := gin.H{
+		"username": "user",
+		"password": "password",
+	}
+
+	res2, err := client.Request("auth/issue", http.MethodPost, reqBody2)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	assert.Equal(t, http.StatusNotFound, res2.StatusCode)
+}
+
+func TestSignInWithWrongPassword(t *testing.T) {
+	db := NewTestDB(&models.User{})
+
+	defer db.Close()
+
+	client := NewSignInTestClient(db)
+
+	defer client.Close()
+
+	reqBody1 := gin.H{
+		"username": "username",
+		"password": "password",
+	}
+
+	res1, err := client.Request("auth", http.MethodPost, reqBody1)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	assert.Equal(t, http.StatusCreated, res1.StatusCode)
+
+	reqBody2 := gin.H{
+		"username": "username",
+		"password": "wrongpass",
+	}
+
+	res2, err := client.Request("auth/issue", http.MethodPost, reqBody2)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	assert.Equal(t, http.StatusUnauthorized, res2.StatusCode)
+}
